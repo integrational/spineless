3be5222409d82271c80c1c96420fe62bb73d8450
GeraldLoeffler <gerald.loeffler@googlemail.com>
2017-10-08 12:16:05 +0200

    correctly support status, type, body and delay for GET

--- src/main/kotlin/org/integrational/spineless/SpinelessResource.kt
+++ src/main/kotlin/org/integrational/spineless/SpinelessResource.kt
@@ -1,8 +1,9 @@
 package org.integrational.spineless
 
+import javax.ws.rs.DefaultValue
 import javax.ws.rs.GET
 import javax.ws.rs.Path
-import javax.ws.rs.Produces
+import javax.ws.rs.QueryParam
 import javax.ws.rs.core.MediaType
 import javax.ws.rs.core.Response
 
@@ -10,9 +11,20 @@ import javax.ws.rs.core.Response
 @Path("/spineless")
 class SpinelessResource {
 
+    object Defaults {
+        const val status = 200
+        const val type = MediaType.APPLICATION_JSON
+        const val body = """{ "status" : "OK"}"""
+        const val delay = 0L
+    }
+
+    // JAX-RS automatically adds support for @HEAD and @OPTIONS
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    fun respond(): Response {
-        return Response.ok("OK").build()
+    fun respond(@QueryParam("status") @DefaultValue(Defaults.status.toString()) status: Int,
+                @QueryParam("type") @DefaultValue(Defaults.type) type: String,
+                @QueryParam("body") @DefaultValue(Defaults.body) body: String,
+                @QueryParam("delay") @DefaultValue(Defaults.delay.toString()) delay: Long): Response {
+        Thread.sleep(delay)
+        return Response.status(status).type(type).entity(body).build()
     }
 }
