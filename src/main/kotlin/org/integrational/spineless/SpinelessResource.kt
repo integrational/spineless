de92941933ce84559ea8c2768bd154582cfe7f76
GeraldLoeffler <gerald.loeffler@googlemail.com>
2017-10-08 13:54:54 +0200

    added support for all relevant HTTP methods and the pathHeader parameter: feature complete for now

--- src/main/kotlin/org/integrational/spineless/SpinelessResource.kt
+++ src/main/kotlin/org/integrational/spineless/SpinelessResource.kt
@@ -1,30 +1,41 @@
 package org.integrational.spineless
 
-import javax.ws.rs.DefaultValue
-import javax.ws.rs.GET
-import javax.ws.rs.Path
-import javax.ws.rs.QueryParam
+import javax.ws.rs.*
 import javax.ws.rs.core.MediaType
 import javax.ws.rs.core.Response
 
 
-@Path("/spineless")
-class SpinelessResource {
+@Path("/spineless/{path}")
+class SpinelessResource(
+        @PathParam("path") @DefaultValue("") private val path: String,
+        @QueryParam("pathHeader") @DefaultValue(Defaults.pathHeader) private val pathHeader: String,
+        @QueryParam("delay") @DefaultValue(Defaults.delay.toString()) private val delay: Long,
+        @QueryParam("status") @DefaultValue(Defaults.status.toString()) private val status: Int,
+        @QueryParam("type") @DefaultValue(Defaults.type) private val type: String,
+        @QueryParam("body") @DefaultValue(Defaults.body) private val body: String) {
 
     object Defaults {
+        const val pathHeader = "X-Spineless-Request-Path"
+        const val delay = 0L
         const val status = 200
         const val type = MediaType.APPLICATION_JSON
         const val body = """{ "status" : "OK"}"""
-        const val delay = 0L
     }
 
-    // JAX-RS automatically adds support for @HEAD and @OPTIONS
-    @GET
-    fun respond(@QueryParam("status") @DefaultValue(Defaults.status.toString()) status: Int,
-                @QueryParam("type") @DefaultValue(Defaults.type) type: String,
-                @QueryParam("body") @DefaultValue(Defaults.body) body: String,
-                @QueryParam("delay") @DefaultValue(Defaults.delay.toString()) delay: Long): Response {
+    @GET // JAX-RS automatically adds support for @HEAD and @OPTIONS
+    fun get() = respond()
+
+    @PUT
+    fun put() = respond()
+
+    @POST
+    fun post() = respond()
+
+    @DELETE
+    fun delete() = respond()
+
+    private fun respond(): Response {
         Thread.sleep(delay)
-        return Response.status(status).type(type).entity(body).build()
+        return Response.status(status).type(type).entity(body).header(pathHeader, path).build()
     }
 }
