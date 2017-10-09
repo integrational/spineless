1c574f3a4629692192a66ebc271453e5097f3d1e
GeraldLoeffler <gerald.loeffler@googlemail.com>
2017-10-09 14:52:53 +0200

    reworked interface: now returning summary of all parameters in header and default body

--- src/main/kotlin/org/integrational/spineless/SpinelessResource.kt
+++ src/main/kotlin/org/integrational/spineless/SpinelessResource.kt
@@ -1,5 +1,7 @@
 package org.integrational.spineless
 
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import org.integrational.spineless.SpinelessResource.Defaults
 import javax.ws.rs.*
 import javax.ws.rs.core.MediaType
 import javax.ws.rs.core.Response
@@ -12,19 +14,21 @@ import javax.ws.rs.core.Response
 @Path("/{path}")
 class SpinelessResource(
         @PathParam("path") @DefaultValue("") private val path: String,
-        @QueryParam("pathHeader") @DefaultValue(Defaults.pathHeader) private val pathHeader: String,
+        @QueryParam("summaryHeader") @DefaultValue(Defaults.summaryHeader) private val summaryHeader: String,
         @QueryParam("delay") @DefaultValue(Defaults.delay.toString()) private val delay: Long,
         @QueryParam("status") @DefaultValue(Defaults.status.toString()) private val status: Int,
         @QueryParam("type") @DefaultValue(Defaults.type) private val type: String,
-        @QueryParam("body") @DefaultValue(Defaults.body) private val body: String) {
+        @QueryParam("body") private val body: String?) {
 
     object Defaults {
-        const val pathHeader = "X-Spineless-Request-Path"
+        const val summaryHeader = "X-Spineless-Summary"
         const val delay = 0L
         const val status = 200
         const val type = MediaType.APPLICATION_JSON
-        const val body = """{ "status" : "OK" }"""
     }
+
+    private val jsonMapper by lazy { jacksonObjectMapper() }
+    private val jsonWriter by lazy { jsonMapper.writer() }
 
     @GET // JAX-RS automatically adds support for @HEAD and @OPTIONS
     fun get() = respond()
@@ -40,6 +44,18 @@ class SpinelessResource(
 
     private fun respond(): Response {
         Thread.sleep(delay)
-        return Response.status(status).type(type).entity(body).header(pathHeader, path).build()
+        val sum = asJson(summary())
+        val entity = body ?: sum
+        return Response.status(status).type(type).entity(entity).header(summaryHeader, sum).build()
     }
+
+    private fun summary() = Summary(summaryHeader, path, delay, status, type, body)
+    private fun asJson(obj: Any): String = jsonWriter.writeValueAsString(obj)
+
+    data class Summary(val summaryHeader: String,
+                       val path: String,
+                       val delay: Long,
+                       val status: Int,
+                       val type: String,
+                       val body: String?)
 }
